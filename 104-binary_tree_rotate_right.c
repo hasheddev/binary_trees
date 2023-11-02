2066ef38ba5fcdf383116ef3559a3d28a05a8fe7
kdshola <khidirshola@gmail.com>
2023-11-02 21:33:22 +0100

    add task files

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -1,10 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_rotate_right - Right-rotates a binary tree.
- * @tree: A pointer to the root node of the tree to rotate.
- *
- * Return: A pointer to the new root node after rotation.
+ * binary_tree_rotate_right - rotates a binary tree right.
+ * @tree: pointer to the root node of the tree to rotate.
+ * Return: A pointer to the new root.
  */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
@@ -23,10 +22,10 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 	pivot->parent = tmp;
 	if (tmp != NULL)
 	{
-		if (tmp->left == tree)
-			tmp->left = pivot;
-		else
+		if (tmp->left != tree)
 			tmp->right = pivot;
+		else
+			tmp->left = pivot;
 	}
 
 	return (pivot);
